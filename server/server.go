b1299c9af2afe9b9db97b3db0c1091a0bdef85a6
DL-NEST <dlnestmit@gmail.com>
2023-02-13 17:53:43 +0800

    结构修改

--- server/server.go
+++ server/server.go
@@ -2,15 +2,12 @@ package server
 
 import (
 	"github.com/spf13/viper"
-	"linktree_core/server/router"
-	"linktree_core/utils/dlog"
-	"linktree_core/utils/gos"
+	"linktree_core/global"
 	"net/http"
 )
 
-// MainServe 启动web服务
-func MainServe() {
-	router := router.MainRouter()
+func StartServe() {
+	Router := MainRouter()
 	// 启动服务
 	if !viper.InConfig("server.port") {
 		return
@@ -18,29 +15,11 @@ func MainServe() {
 	port := viper.GetString("server.port")
 	srv := &http.Server{
 		Addr:    ":" + port,
-		Handler: router,
+		Handler: Router,
 	}
 	err := srv.ListenAndServe()
 	if err != nil {
-		dlog.Log.Panicf("服务启动失败:%v", err)
-		return
-	}
-}
-
-// ConfigServe 启动一个初始化配置文件的服务
-func ConfigServe() {
-	dlog.Log.Warnf("未找到配置文件,启动初始化服务")
-	// 生成随机密码
-	gos.FirstPassFile()
-	router := router.InitRouter()
-	port := viper.GetString("server.port")
-	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
-	}
-	err := srv.ListenAndServe()
-	if err != nil {
-		dlog.Log.Panicf("服务启动失败:%v", err)
+		global.GLOG.Panicf("服务启动失败:%v", err)
 		return
 	}
 }
